c066024ae7847b3ea76e56660b49d57b7528e740
John Rozeske <john@rozeske.com>
2010-10-31 17:39:58 -0500

    Notification dialog

---
+++ notify.js
@@ -0,0 +1,144 @@
+YAHOO.namespace("lacuna");
+
+if (typeof YAHOO.lacuna.Notify == "undefined" || !YAHOO.lacuna.Notify) {
+	
+(function(){
+	var Lang = YAHOO.lang,
+		Util = YAHOO.util,
+		Dom = Util.Dom,
+		Event = Util.Event,
+		Lacuna = YAHOO.lacuna,
+		Game = Lacuna.Game,
+		Lib = Lacuna.Library;
+		
+	var Notify = function(){
+		this.incomingShips = {};
+	};
+	Notify.prototype = {
+		_createDisplay : function() {
+			if(!this.Display) {
+				var container = document.createElement("div");
+				container.id = "notify";
+				Dom.addClass(container, Lib.Styles.HIDDEN);
+				Dom.addClass(container, "nofooter");
+				container.innerHTML = this._getHtml();
+				document.body.insertBefore(container, document.body.firstChild);
+				
+				this.Display = new YAHOO.widget.Panel("notify", {
+					constraintoviewport:true,
+					visible:false,
+					draggable:true,
+					effect:Game.GetContainerEffect(),
+					close:true,
+					underlay:false,
+					modal:false,
+					width:"350px",
+					context:["header","tr","br", ["beforeShow", "windowResize"], [0,40]]
+				});
+				this.Display.renderEvent.subscribe(function(){
+					this.notifyList = Dom.get('notifyList');
+					this.notify = Dom.get("notify");
+					/*this.notifyHide = document.createElement("div");
+					this.notifyHide.id = "notifyHide";
+					Dom.setStyle(this.notifyHide, "background-color", "red");
+					this.notify.parentNode.insertBefore(this.notifyHide, this.notify);*/
+					
+					Dom.removeClass(this.notify, Lib.Styles.HIDDEN);
+				});
+				/*this.Display.showEvent.subscribe(function(){
+					Dom.setStyle(this.notifyHide, "visibility", "hidden");
+				});
+				this.Display.beforeHideEvent.subscribe(function(){
+					Dom.setStyle(this.notifyHide, "visibility", "visible");
+					Dom.setStyle(this.notify, "visibility", "hidden");
+					return false;
+				});*/
+				this.Display.render();
+			}
+		},
+		_getHtml : function() {
+			return [
+			'	<div class="hd">Alerts</div>',
+			'	<div class="bd" style="background:#FFD800;color:#000;">',
+			'		<ul id="notifyList">',
+			'		</ul>',
+			'	</div>',
+			].join('');
+		},
+		_updating : function() {
+			var list = this.Display.notifyList,
+				parent = list.parentNode;
+			list = parent.removeChild(list);
+			
+			var planetShips = this.incomingShips[this.planetId], 
+				p = Game.EmpireData.planets[this.planetId],
+				arr = ['<li style="border-bottom:1px solid #52ACFF;font-weight:bold;">Incoming Ships To ',p.name,'!</li>'],
+				i = 0;
+			if(planetShips.length === 0) {
+				arr = arr.concat(['<li>None</li>']);
+			}
+			else {
+				for(var s=0; s<planetShips.length;s++) {
+					var ship = planetShips[s],
+						ms = Lib.parseServerDate(ship.date_arrives) - (new Date());
+					if(ms > 0) {
+						i++;
+						arr = arr.concat(['<li>',Lib.formatMillisecondTime(ms),'</li>']);
+					}
+				}
+			}
+			
+			list.innerHTML = arr.join('');
+
+			parent.appendChild(list);
+			
+			if(i == 0) {
+				Game.onTick.unsubscribe(this._updating);
+				delete this.subscribed;
+				this.incomingShips = {};
+			}
+		},
+		Load : function(planet) {
+			var incoming = planet.incoming_foreign_ships || [],
+				planetShips = this.incomingShips[planet.id] || [];
+			
+			if(planetShips.length != incoming.length) {
+				this._createDisplay();
+				this.incomingShips[planet.id] = incoming;
+				this.planetId = planet.id;
+
+				if(!this.subscribed) {
+					Game.onTick.subscribe(this._updating, this, true);
+					this.subscribed = 1;
+				}
+				
+				this.Display.show();
+				this.Display.bringToTop();
+			}
+		},
+		Show : function(planetId) {
+			if(this.Display) {
+				if(this.subscribed && this.incomingShips[planetId]) {
+					this.planetId = planetId;
+					this.Display.show();
+					this.Display.bringToTop();
+				}
+				else {
+					this.Display.hide();
+				}
+			}
+		},
+		Hide : function() {
+			if(this.Display) {
+				this.Display.hide();
+			}
+			delete this.planetId;
+		}
+	};
+	
+	Lacuna.Notify = new Notify();
+		
+})();
+YAHOO.register("notify", YAHOO.lacuna.Notify, {version: "1", build: "0"}); 
+
+}
