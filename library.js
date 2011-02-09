55b211236c0c2697ca9e3fdef2af994d172e2aad
Graham Knop <haarg@haarg.org>
2011-02-09 11:38:30 -0600

    update error code list

--- library.js
+++ library.js
@@ -107,14 +107,19 @@ if (typeof YAHOO.lacuna.Library == "undefined" || !YAHOO.lacuna.Library) {
 			1003 : "Too much information",
 			1004 : "Password incorrect",
 			1005 : "Contains invalid characters",
-			1006 : "Authorization denied",
+			1006 : "Session expired",
 			1007 : "Overspend",
 			1008 : "Underspend",
 			1009 : "Invalid range",
 			1010 : "Insufficient privileges",
 			1011 : "Not enough resources in storage",
 			1012 : "Not enough resources in production",
-			1013 : "Missing prerequisites" 
+			1013 : "Missing prerequisites",
+			1014 : "Captcha not valid",
+			1015 : "Restricted for sitter logins",
+			1100 : "Empire not founded",
+			1101 : "Empire not founded, and you tried to create it, but had the wrong password",
+			1200 : "Game Over"
 		},
 		View : {
 			STAR : "star",
