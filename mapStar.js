ddf1117ce95600990ffb0e40eccc038bbe4bd304
lemming552 <move@quirkyqatz.com>
2012-02-25 21:45:46 -0700

    Station Name in Star Info displayed.

--- mapStar.js
+++ mapStar.js
@@ -1249,6 +1249,7 @@ if (typeof YAHOO.lacuna.MapStar == "undefined" || !YAHOO.lacuna.MapStar) {
 				'	<li><label>X: </label>',data.x,'</li>',
 				'	<li><label>Y: </label>',data.y,'</li>',
 				'	<li><label>Zone: </label>',data.zone,'</li>',
+				data.station ? ('	<li><label>Station: </label>'+data.station.name+'</li>') : '',
 				'</ul>'
 			].join('');
 			
