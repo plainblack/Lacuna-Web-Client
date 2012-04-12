123601d5885cfe1cfc0cf0d7a7398752ab684867
lemming552 <lemming@quirkyqatz.com>
2012-04-12 15:42:31 -0600

    Still broken, but gets task list.

--- buildingBlackHoleGenerator.js
+++ buildingBlackHoleGenerator.js
@@ -1,136 +1,239 @@
 YAHOO.namespace("lacuna.buildings");
 
-if (typeof YAHOO.lacuna.buildings.BlackHoleGenerator == "undefined" || !YAHOO.lacuna.buildings.BlackHoleGenerator) {
-	
+if (typeof YAHOO.lacuna.buildings.BlackHoleGenerator == "undefined" ||
+           !YAHOO.lacuna.buildings.BlackHoleGenerator) {
+  
 (function(){
-	var Lang = YAHOO.lang,
-		Util = YAHOO.util,
-		Dom = Util.Dom,
-		Event = Util.Event,
-		Sel = Util.Selector,
-		Pager = YAHOO.widget.Paginator,
-		Lacuna = YAHOO.lacuna,
-		Game = Lacuna.Game,
-		Lib = Lacuna.Library;
+  var Lang = YAHOO.lang,
+    Util = YAHOO.util,
+    Dom = Util.Dom,
+    Event = Util.Event,
+    Sel = Util.Selector,
+    Pager = YAHOO.widget.Paginator,
+    Lacuna = YAHOO.lacuna,
+    Game = Lacuna.Game,
+    Lib = Lacuna.Library;
 
-	var BlackHoleGenerator = function(result){
-		BlackHoleGenerator.superclass.constructor.call(this, result);
-		
-		this.service = Game.Services.Buildings.BlackHoleGenerator;
-	};
-	
-	Lang.extend(BlackHoleGenerator, Lacuna.buildings.Building, {
-		getChildTabs : function() {
-			return [this._getTab()];
-		},
-		_getTab : function() {
-            var tasks = this.result.tasks;
+  var BlackHoleGenerator = function(result){
+    BlackHoleGenerator.superclass.constructor.call(this, result);
+    
+    this.service = Game.Services.Buildings.BlackHoleGenerator;
+  };
+  
+  Lang.extend(BlackHoleGenerator, Lacuna.buildings.Building, {
+    getChildTabs : function() {
+      return [this._getBHGTab()];
+    },
+    _getBHGTab : function() {
+      this.tab = new YAHOO.widget.Tab({ label: "Singularity", content: [
+        '<div id="bhg">',
+        '   <div id="bhgTaskPick">',
+        '      Target <select id="bhgTargetType">',
+        '        <option value="body_name">Body Name</option>',
+        '        <option value="body_id">Body Id</option>',
+        '        <option value="xy">X,Y</option>',
+        '      </select>',
+        '      <span id="bhgTargetSelectText"><input type="text" id="bhgTargetText" /></span>',
+        '      <span id="bhgTargetSelectXY" style="display:none;">',
+        '        X:<input size="5" type="text" id="bhgTargetX" />',
+        '        Y:<input size="5" type="text" id="bhgTargetY" />',
+        '      </span>',
+        '      <button type="button" id="bhgGetActions">Get Actions</button>',
+        '   </div>',
+        '   <div id="bhgTaskInfo"></div>',
+        '   <div id="bhgActions" style="display:none;border-top:1px solid #52ACFF;margin-top:5px;padding-top:5px">',
+        '  Singularity Target: <span id="bhgTargetNote"></span><span id="bhgTargetDist"></span>',
+        '  <div style="border-top:1px solid #52ACFF;margin-top:5px;"><ul id="bhgActionsAvail"></ul></div>',
+        '  <div id="bhgResult"></div>',
+        '</div>'
+      ].join('')});
 
-			this.tab = new YAHOO.widget.Tab({ label: "Singularity", content: [
-				'<div id="singularity">',
-				'   <div id="singularityTask">',
-				'      Task ' + this.selectTask(tasks),
-				'      Target <select id="singularityTargetType"><option value="body_name">Body Name</option><option value="body_id">Body Id</option><option value="xy">X,Y</option></select>',
-				'      <span id="singularityTargetText"><input type="text" id="singularityTargetText" /></span>',
-				'      <span id="singularityTargetXY" style="display:none;">X:<input size="5" type="text" id="singularityTargetX" /> Y:<input size="5" type="text" id="singularityTarget Y" /></span>',
-				'      <span id="singularityNewType" style="display:none;">New Type:<input size="3" type="text" id="singularityNewType" /></span>',
-				'      <button type="button" id="singularityGenerate">Generate</button>',
-				'   </div>',
-				'   <div id="singularityTaskInfo"></div>',
-				'   <div id="singularityResult">',
-				'   </div>',
-				'</div>'
-			].join('')});
+      Event.on("bhgTargetType", "change", function(){
+        if(Lib.getSelectedOptionValue(this) == "xy") {
+          Dom.setStyle("bhgTargetSelectText", "display", "none");
+          Dom.setStyle("bhgTargetSelectXY", "display", "");
+        }
+        else {
+          Dom.setStyle("bhgTargetSelectText", "display", "");
+          Dom.setStyle("bhgTargetSelectXY", "display", "none");
+        }
+      });
+      Event.on("bhgGetActions", "click", this.bhgGetActions, this, true);
+      return this.tab;
+    },
+    bhgGetActions : function() {
+      Lacuna.Pulser.Show();
+      
+      Dom.setStyle("bhgActions", "display", "none");
+      
+      var type = Lib.getSelectedOptionValue("bhgTargetType"),
+          target = {};
+      
+      if(type == "xy") {
+        target.x = Dom.get("bhgTargetX").value;
+        target.y = Dom.get("bhgTargetY").value;
+        Dom.get("bhgTargetNote").innerHTML = ['X: ', target.x, ', Y: ', target.y].join('');
+        target.method = "xy";
+      }
+      else {
+        target[type] = Dom.get("bhgTargetText").value;
+        Dom.get("bhgTargetNote").innerHTML = target[type];
+        target.method = type;
+      }
+      
+      this.service.get_actions_for({
+        session_id:Game.GetSession(),
+        building_id:this.building.id,
+        target:target
+      }, {
+        success : function(o){
+          Lacuna.Pulser.Hide();
+          this.rpcSuccess(o);
+          this.PopulateBHGTab(target, o.result.tasks);
+        },
+        scope:this
+      });
+    },
+    PopulateBHGTab : function(target, actions) {
+      var details = Dom.get("bhgActionsAvail"),
+          detailsParent = details.parentNode,
+          li = document.createElement("li");
 
-			Event.on("singularityTargetType", "change", function(){
-				if(Lib.getSelectedOptionValue(this) == "xy") {
-					Dom.setStyle("singularityTargetText", "display", "none");
-					Dom.setStyle("singularityTargetXY", "display", "");
-				}
-				else {
-					Dom.setStyle("singularityTargetText", "display", "");
-					Dom.setStyle("singularityTargetXY", "display", "none");
-				}
-			});
-			Event.on("singularityTaskSelector", "change", function(){
-				var task = Lib.getSelectedOptionValue(this);
-				if(task == "Change Type") {
-					Dom.setStyle("singularityNewType", "display", "");
-				}
-				else {
-					Dom.setStyle("singularityNewType", "display", "none");
-				}
-				var taskInfo;
-				for(var i=0; i<tasks.length; i++) {
-					if (tasks[i]["name"] == task) {
-						taskInfo = tasks[i];
-						break;
-					}
-				}
-				var div = Dom.get("singularityTaskInfo");
-				div.innerHTML = '';
-				if (taskInfo) {
-					if (taskInfo["reason"] && taskInfo["reason"] != "Invalid reason.") {
-						div.appendChild(document.createTextNode(taskInfo["reason"]));
-						div.appendChild(document.createElement("br"));
-					}
-					if (taskInfo["waste_cost"] || taskInfo["min_level"]) {
-						var req = '';
-						if (taskInfo["waste_cost"]) {
-							req += 'Waste Cost: ' + Lib.convertNumDisplay(taskInfo["waste_cost"]);
-						}
-						if (taskInfo["min_level"]) {
-                            if (req.length) req += " \u00a0 ";
-							req += 'Minimum Level: ' + taskInfo["min_level"];
-						}
-						
-						div.appendChild(document.createTextNode(req));
-						div.appendChild(document.createElement("br"));
-					}
-					if (taskInfo["recovery"]) {
-						div.appendChild(document.createTextNode('Recovery Time: ' + Lib.formatTime(taskInfo["recovery"])));
-						div.appendChild(document.createElement("br"));
-					}
-					if (taskInfo["fail_chance"] || taskInfo["side_chance"]) {
-						var odds = '';
-						if (taskInfo["fail_chance"]) {
-							odds += 'Chance of Failure (before considering range): ' + taskInfo["fail_chance"] + '%';
-						}
-						if (taskInfo["side_chance"]) {
-                            if (odds.length) odds += " \u00a0 ";
-							odds += 'of Side effect: ' + taskInfo["side_chance"] + '%';
-						}
-						
-						div.appendChild(document.createTextNode(odds));
-						div.appendChild(document.createElement("br"));
-					}
-				}
-			});
-			Event.on("singularityGenerate", "click", function(){window.alert("UI not yet completed")}, this, true);
+        
+      Event.purgeElement(details, true); //clear any events before we remove
+      details = detailsParent.removeChild(details); //remove from DOM to make this faster
+      details.innerHTML = "";
+      
+      Dom.setStyle("bhgActions", "display", "");
+      
+      if(actions.length === 0) {
+        details.innerHTML = "No available actions for singularity.";
+      }
+      else {        
+        Dom.get("bhgTargetDist").innerHTML = [
+          ' ; Range: ', actions[0].range, ' Dist: ', actions[0].dist, 'The Generate Button does not work'].join('');
+        for(var i=0; i<actions.length; i++) {
+          var task = actions[i],
+              nLi = li.cloneNode(false);
+            
+          nLi.Task = task;
+          nLi.innerHTML = ['<div class="yui-gd" style="margin-bottom:2px;">',
+          '  <div style="border:2px gold solid;" class="yui-u" style="width:80%">',
+          task.success > 0 ? '    <button type="button" id="bhgGenerateButton">Generate</button>' : '',
+          '    <label style="font-weight:bold;">',task.name,'</label>',
+          task.name === "Change Type" ? 
+          '  <span id="bhgChangeTypeSelectText"><b>:</b><input type="number" id="bhgChangeTypeText" /></span>' : '',
+          '    <div>',
+          '      <span>Base Chance:<span>',100-task.base_fail,'</span></span>%,',
+          '      <span>Success Chance:<span>',task.success,'</span></span>%,',
+          '      <span>Waste Needed:<span>',Lib.formatNumber(task.waste_cost),' </span></span>',
+          '      <span>Recovery Time:<span>',Lib.formatTime(task.recovery),'</span></span>',
+          '    </div>',
+          '    <div>',
+          '    </div>',
+          '  </div>',
+          '</div>'].join('');
+          
 
-			return this.tab;
-		},
-		
-		selectTask : function(tasks) {
-			var sel = document.createElement("select"),
-				opt = document.createElement("option");
-			sel.appendChild(opt);
-			sel.id = "singularityTaskSelector";
-			for(var i=0; i<tasks.length; i++) {
-				var nOpt = opt.cloneNode(false);
-				nOpt.value = nOpt.innerHTML = tasks[i]["name"];
-				sel.appendChild(nOpt);
-			}
+          if(task.success > 0) {
+            if (task.name === "Change Type") {
+                Dom.setStyle("bhgChangeTypeSelectText", "display", "none");
+            }
+          }
+          
+          details.appendChild(nLi);
+        }
+      }
+      detailsParent.appendChild(details); //add back as child
 
-			var span = document.createElement("span");
-			span.appendChild(sel);
+      //wait for tab to display first
+      setTimeout(function() {
+        var Ht = Game.GetSize().h - 250;
+        if(Ht > 250) { Ht = 250; }
+        Dom.setStyle(detailsParent,"height",Ht + "px");
+        Dom.setStyle(detailsParent,"overflow-y","auto");
+      },10);
+      return this.tab;
+    },
+    bhgGenerate   : function(oself, mytask, target) {
+       var params = {};
 
-			return span.innerHTML;
-		}
-		
-	});
-	
-	YAHOO.lacuna.buildings.BlackHoleGenerator = BlackHoleGenerator;
+      
+      params.newtype = 11;
+      if (mytask === "Change Type") {
+        params.newtype = Lib.getSelectedOptionValue("bhgChangeTypeText");
+      }
+      else {
+        params.newtype = 9;
+      }
+      alert('task: ' + mytask + '\n' +
+            'target: ' + target.method + '\n' +
+            'params: ' + params.newtype);
+      if (target) {
+        this.service.generate_singularity({
+          session_id:Game.GetSession(),
+          building_id:oself.building.id,
+          target:target,
+          task:mytask,
+          params:params,
+        }, {
+        success : function(o){
+          Lacuna.Pulser.Hide();
+          this.rpcSuccess(o);
+          this.PopulateBHGResult(target, o.result.effect);
+        },
+          scope:this
+        });
+      }
+      else {
+        btn.disabled = false;
+      }
+    },
+    PopulateBHGResult : function(target, effect) {
+      var details = Dom.get("bhgResult"),
+        detailsParent = details.parentNode,
+        li = document.createElement("li");
+        
+      Event.purgeElement(details, true); //clear any events before we remove
+      details = detailsParent.removeChild(details); //remove from DOM to make this faster
+      details.innerHTML = "";
+      
+      Dom.setStyle("bhgResult", "display", "");
+      detailsParent.appendChild(details); //add back as child
+      
+      if (effect.target) {
+        var details = effect.target;
+        nLi.innerHTML = [ '<div class="yui-gd" style="margin-bottom:2px;">',
+          '  <div style="border:2px gold solid;" class="yui-u" style="width:67%">',
+          '    <div><label style="font-weight:bold;">',details.message,'</label></div>',
+        ].join('');
+      }
+      if (effect.side) {
+        var details = effect.target;
+        nLi.innerHTML = [ '<div class="yui-gd" style="margin-bottom:2px;">',
+          '  <div style="border:2px gold solid;" class="yui-u" style="width:67%">',
+          '    <div><label style="font-weight:bold;">',details.message,'</label></div>',
+        ].join('');
+      }
+      if (effect.fail) {
+        var details = effect.target;
+        nLi.innerHTML = [ '<div class="yui-gd" style="margin-bottom:2px;">',
+          '  <div style="border:2px gold solid;" class="yui-u" style="width:67%">',
+          '    <div><label style="font-weight:bold;">',details.message,'</label></div>',
+        ].join('');
+      }
+              
+      //wait for tab to display first
+      setTimeout(function() {
+        var Ht = Game.GetSize().h - 250;
+        if(Ht > 250) { Ht = 250; }
+        Dom.setStyle(detailsParent,"height",Ht + "px");
+        Dom.setStyle(detailsParent,"overflow-y","auto");
+      },10);
+    }
+  });
+  
+  YAHOO.lacuna.buildings.BlackHoleGenerator = BlackHoleGenerator;
 
 })();
 YAHOO.register("blackholegenerator", YAHOO.lacuna.buildings.BlackHoleGenerator, {version: "1", build: "0"}); 
