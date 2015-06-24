e2d51db489faea7620ef4afa0446f946790c2b89
1vasari <onevasari@gmail.com>
2015-06-24 12:41:50 +1000

    Sorted out the Notes window's behaviour.

--- app/js/stores/rpc/body/notes.js
+++ app/js/stores/rpc/body/notes.js
@@ -19,6 +19,51 @@ var NotesDataStore = Reflux.createStore({
     init: function() {
         // Use this to store the notes before they get saved.
         this.data = '';
+
+        //                          .i;;;;i.
+        //                        iYcviii;vXY:
+        //                      .YXi       .i1c.
+        //                     .YC.     .    in7.
+        //                    .vc.   ......   ;1c.
+        //                    i7,   ..        .;1;
+        //                   i7,   .. ...      .Y1i
+        //                  ,7v     .6MMM@;     .YX,
+        //                 .7;.   ..IMMMMMM1     :t7.
+        //                .;Y.     ;$MMMMMM9.     :tc.
+        //                vY.   .. .nMMM@MMU.      ;1v.
+        //               i7i   ...  .#MM@M@C. .....:71i
+        //              it:   ....   $MMM@9;.,i;;;i,;tti
+        //             :t7.  .....   0MMMWv.,iii:::,,;St.
+        //            .nC.   .....   IMMMQ..,::::::,.,czX.
+        //           .ct:   ....... .ZMMMI..,:::::::,,:76Y.
+        //           c2:   ......,i..Y$M@t..:::::::,,..inZY
+        //          vov   ......:ii..c$MBc..,,,,,,,,,,..iI9i
+        //         i9Y   ......iii:..7@MA,..,,,,,,,,,....;AA:
+        //        iIS.  ......:ii::..;@MI....,............;Ez.
+        //       .I9.  ......:i::::...8M1..................C0z.
+        //      .z9;  ......:i::::,.. .i:...................zWX.
+        //      vbv  ......,i::::,,.      ................. :AQY
+        //     c6Y.  .,...,::::,,..:t0@@QY. ................ :8bi
+        //    :6S. ..,,...,:::,,,..EMMMMMMI. ............... .;bZ,
+        //   :6o,  .,,,,..:::,,,..i#MMMMMM#v.................  YW2.
+        //  .n8i ..,,,,,,,::,,,,.. tMMMMM@C:.................. .1Wn
+        //  7Uc. .:::,,,,,::,,,,..   i1t;,..................... .UEi
+        //  7C...::::::::::::,,,,..        ....................  vSi.
+        //  ;1;...,,::::::,.........       ..................    Yz:
+        //   v97,.........                                     .voC.
+        //    izAotX7777777777777777777777777777777777777777Y7n92:
+        //      .;CoIIIIIUAA666666699999ZZZZZZZZZZZZZZZZZZZZ6ov.
+
+        // THIS CODE IS TERRIBLE. IT IS NOT GOOD REACT CODE. DO NOT COPY IT.
+        this.planetId = undefined;
+        BodyRPCStore.listen(function(body) {
+            if (body.id !== this.planetId) {
+                // We changed planet. The save automagically happend below. We just need to
+                // bring the new data in.
+                this.planetId = body.id;
+                NotesActions.set(body.notes);
+            }
+        }, this);
     },
 
     getInitialState: function() {
@@ -35,7 +80,9 @@ var NotesDataStore = Reflux.createStore({
     },
 
     onLoad: function() {
-        this.trigger(BodyRPCStore.getData().notes);
+        var data = BodyRPCStore.getData();
+        this.planetId = data.id;
+        this.trigger(data.notes);
     },
 
     onClear: function() {
@@ -53,7 +100,7 @@ var NotesDataStore = Reflux.createStore({
             method: 'set_colony_notes',
             trigger: false,
             params: [
-                BodyRPCStore.getData().id,
+                this.planetId,
                 {
                     notes: this.data
                 }
