73d57016823573925af94e63b3baa31916ade82e
Darin McBride <dmcbride@cpan.org>
2015-08-25 16:50:23 -0600

    Clean up comments.

--- app/js/components/menu/map.jsx
+++ app/js/components/menu/map.jsx
@@ -8,43 +8,7 @@ var MapModeStore = require('js/stores/menu/mapMode');
 var PlanetStore = require('js/stores/menu/planet');
 var MenuStore = require('js/stores/menu');
 
-
-//                          .i;;;;i.
-//                        iYcviii;vXY:
-//                      .YXi       .i1c.
-//                     .YC.     .    in7.
-//                    .vc.   ......   ;1c.
-//                    i7,   ..        .;1;
-//                   i7,   .. ...      .Y1i
-//                  ,7v     .6MMM@;     .YX,
-//                 .7;.   ..IMMMMMM1     :t7.
-//                .;Y.     ;$MMMMMM9.     :tc.
-//                vY.   .. .nMMM@MMU.      ;1v.
-//               i7i   ...  .#MM@M@C. .....:71i
-//              it:   ....   $MMM@9;.,i;;;i,;tti
-//             :t7.  .....   0MMMWv.,iii:::,,;St.
-//            .nC.   .....   IMMMQ..,::::::,.,czX.
-//           .ct:   ....... .ZMMMI..,:::::::,,:76Y.
-//           c2:   ......,i..Y$M@t..:::::::,,..inZY
-//          vov   ......:ii..c$MBc..,,,,,,,,,,..iI9i
-//         i9Y   ......iii:..7@MA,..,,,,,,,,,....;AA:
-//        iIS.  ......:ii::..;@MI....,............;Ez.
-//       .I9.  ......:i::::...8M1..................C0z.
-//      .z9;  ......:i::::,.. .i:...................zWX.
-//      vbv  ......,i::::,,.      ................. :AQY
-//     c6Y.  .,...,::::,,..:t0@@QY. ................ :8bi
-//    :6S. ..,,...,:::,,,..EMMMMMMI. ............... .;bZ,
-//   :6o,  .,,,,..:::,,,..i#MMMMMM#v.................  YW2.
-//  .n8i ..,,,,,,,::,,,,.. tMMMMM@C:.................. .1Wn
-//  7Uc. .:::,,,,,::,,,,..   i1t;,..................... .UEi
-//  7C...::::::::::::,,,,..        ....................  vSi.
-//  ;1;...,,::::::,.........       ..................    Yz:
-//   v97,.........                                     .voC.
-//    izAotX7777777777777777777777777777777777777777Y7n92:
-//      .;CoIIIIIUAA666666699999ZZZZZZZZZZZZZZZZZZZZ6ov.
-
-// THIS CODE IS TERRIBLE. IT IS NOT GOOD REACT CODE. DO NOT COPY IT.
-// TODO: factor out all this crappy glue code!
+// TODO: factor out all this glue code
 
 var Map = React.createClass({
     mixins: [
@@ -102,7 +66,7 @@ var Map = React.createClass({
             Lacuna.MapPlanet.MapVisible(true);
             Lacuna.MapPlanet.Load(this.state.planet, true);
 
-            // Sadly, we have to pull hacky tricks like this to avoid infinate loops.
+            // Sadly, we have to pull hacky tricks like this to avoid infinite loops.
             this.previousPlanetId = this.state.planet;
             this.previousMapMode = this.state.mapMode;
 
@@ -120,7 +84,7 @@ var Map = React.createClass({
             Lacuna.MapStar.Load();
             Lacuna.MapStar.Jump(this.state.body.x, this.state.body.y);
 
-            // Sadly, we have to pull hacky tricks like this to avoid infinate loops.
+            // Sadly, we have to pull hacky tricks like this to avoid infinite loops.
             this.previousPlanetId = this.state.planet;
             this.previousMapMode = this.state.mapMode;
 
@@ -134,40 +98,3 @@ var Map = React.createClass({
 });
 
 module.exports = Map;
-
-//                          .i;;;;i.
-//                        iYcviii;vXY:
-//                      .YXi       .i1c.
-//                     .YC.     .    in7.
-//                    .vc.   ......   ;1c.
-//                    i7,   ..        .;1;
-//                   i7,   .. ...      .Y1i
-//                  ,7v     .6MMM@;     .YX,
-//                 .7;.   ..IMMMMMM1     :t7.
-//                .;Y.     ;$MMMMMM9.     :tc.
-//                vY.   .. .nMMM@MMU.      ;1v.
-//               i7i   ...  .#MM@M@C. .....:71i
-//              it:   ....   $MMM@9;.,i;;;i,;tti
-//             :t7.  .....   0MMMWv.,iii:::,,;St.
-//            .nC.   .....   IMMMQ..,::::::,.,czX.
-//           .ct:   ....... .ZMMMI..,:::::::,,:76Y.
-//           c2:   ......,i..Y$M@t..:::::::,,..inZY
-//          vov   ......:ii..c$MBc..,,,,,,,,,,..iI9i
-//         i9Y   ......iii:..7@MA,..,,,,,,,,,....;AA:
-//        iIS.  ......:ii::..;@MI....,............;Ez.
-//       .I9.  ......:i::::...8M1..................C0z.
-//      .z9;  ......:i::::,.. .i:...................zWX.
-//      vbv  ......,i::::,,.      ................. :AQY
-//     c6Y.  .,...,::::,,..:t0@@QY. ................ :8bi
-//    :6S. ..,,...,:::,,,..EMMMMMMI. ............... .;bZ,
-//   :6o,  .,,,,..:::,,,..i#MMMMMM#v.................  YW2.
-//  .n8i ..,,,,,,,::,,,,.. tMMMMM@C:.................. .1Wn
-//  7Uc. .:::,,,,,::,,,,..   i1t;,..................... .UEi
-//  7C...::::::::::::,,,,..        ....................  vSi.
-//  ;1;...,,::::::,.........       ..................    Yz:
-//   v97,.........                                     .voC.
-//    izAotX7777777777777777777777777777777777777777Y7n92:
-//      .;CoIIIIIUAA666666699999ZZZZZZZZZZZZZZZZZZZZ6ov.
-
-// THIS CODE IS TERRIBLE. IT IS NOT GOOD REACT CODE. DO NOT COPY IT.
-// TODO: factor out all this crappy glue code!
