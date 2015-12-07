22138cde90ff0d973d9e493a3d9879a3cc5e629b
1vasari <onevasari@gmail.com>
2015-12-07 22:24:54 +1000

    Scroll dev min build queue window. Fixes #116

--- app/js/building/development.js
+++ app/js/building/development.js
@@ -1,7 +1,7 @@
 YAHOO.namespace("lacuna.buildings");
 
 if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.buildings.Development) {
-    
+
 (function(){
     var Lang = YAHOO.lang,
         Util = YAHOO.util,
@@ -14,10 +14,10 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
 
     var Development = function(result){
         Development.superclass.constructor.call(this, result);
-        
+
         this.service = Game.Services.Buildings.Development;
     };
-    
+
     Lang.extend(Development, Lacuna.buildings.Building, {
         getChildTabs : function() {
             if(this.result.build_queue && this.result.build_queue.length > 0) {
@@ -31,9 +31,9 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
                 div = document.createElement("div"),
                 subDiv = div.cloneNode(false),
                 hUl = ul.cloneNode(false);
-                
+
             Dom.addClass(div, "buildingDetailsExtra");
-            
+
             subDiv.appendChild(document.createTextNode('You can subsidize the build queue for '+this.result.subsidy_cost+' Essentia to finish immediately.'));
             var btn = document.createElement("button");
             btn.setAttribute("type", "button");
@@ -45,7 +45,7 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
             Dom.addClass(hUl, "buildQueue buildQueueHeader clearafter");
             hUl.innerHTML = '<li class="buildQueueRank"># </li><li class="buildQueueName">Building</li><li class="buildQueueLevel">Level</li><li class="buildQueueTime">Time</li><li class="buildQueueCoords">Coordinates</li><li class="buildQueueSubsidy">Subsidy</li><li class="buildQueueAction">Action</li>';
             div.appendChild(hUl);
-            
+
             for(var i=0; i<bq.length; i++) {
                 var bqo = bq[i],
                     nUl = ul.cloneNode(false),
@@ -61,12 +61,12 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
                 Dom.addClass(nLi,"buildQueueName");
                 nLi.innerHTML = bqo.name;
                 nUl.appendChild(nLi);
-                
+
                 nLi = li.cloneNode(false);
                 Dom.addClass(nLi,"buildQueueLevel");
                 nLi.innerHTML = bqo.to_level;
                 nUl.appendChild(nLi);
-                
+
                 var tLi = li.cloneNode(false);
                 Dom.addClass(tLi,"buildQueueTime");
                 tLi.innerHTML = Lib.formatTime(bqo.seconds_remaining);
@@ -98,14 +98,14 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
 
 
                 div.appendChild(nUl);
-                
+
                 this.addQueue(bqo.seconds_remaining, this.DevMinistryQueue, tLi);
             }
-                    
+
             var tab = new YAHOO.widget.Tab({ label: "Build Queue", contentEl: div});
-                    
+
             this.queueTab = tab;
-            
+
             return tab;
         },
 
@@ -172,7 +172,7 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
 
         DevSubsidize : function(e) {
             require('js/actions/menu/loader').show();
-            
+
             this.service.subsidize_build_queue({
                 session_id:Game.GetSession(),
                 building_id:this.building.id
@@ -183,12 +183,12 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
                     var e = Game.EmpireData.essentia*1;
                     Game.EmpireData.essentia = e - o.result.essentia_spent*1;
                     this.rpcSuccess(o);
-                    
+
                     if(this.queueTab) {
                         Event.purgeElement(this.queueTab.get("contentEl"));
                         this.removeTab(this.queueTab);
                     }
-                    
+
                     //close buildingDetails
                     this.fireEvent("onHide");
 
@@ -196,14 +196,14 @@ if (typeof YAHOO.lacuna.buildings.Development == "undefined" || !YAHOO.lacuna.bu
                     this.fireEvent("onUpdateMap");
                 },
                 scope:this
-            });        
+            });
         }
     });
-    
+
     YAHOO.lacuna.buildings.Development = Development;
 
 })();
-YAHOO.register("development", YAHOO.lacuna.buildings.Development, {version: "1", build: "0"}); 
+YAHOO.register("development", YAHOO.lacuna.buildings.Development, {version: "1", build: "0"});
 
 }
 // vim: noet:ts=4:sw=4
