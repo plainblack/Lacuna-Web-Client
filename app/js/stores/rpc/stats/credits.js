d0b06f6afb8a1b5f86d783b9e512e3fe4f0b3139
1vasari <onevasari@gmail.com>
2015-12-20 15:32:56 +1000

    Did some refactoring of the About window code.

--- app/js/stores/rpc/stats/credits.js
+++ app/js/stores/rpc/stats/credits.js
@@ -9,18 +9,67 @@ var server = require('js/server');
 var CreditsRPCStore = Reflux.createStore({
     listenables: AboutActions,
 
+    init: function() {
+        this.data = this.getInitialState();
+    },
+
     getInitialState: function() {
         return [];
     },
 
+    hasData: function() {
+        return _.keys(this.data) > 0;
+    },
+
+    // INPUT:
+    //
+    // [
+    //     { 'Game Server' : ['JT Smith']},
+    //     { 'iPhone Client' : ['Kevin Runde']},
+    //     { 'Web Client' : ['John Rozeske']},
+    //     { 'Play Testers' : ['John Ottinger', 'Jamie Vrbsky']}
+    // ]
+    //
+    // OUTPUT
+    // {
+    //     'Game Server': ['JT Smith'],
+    //     'iPhone Client': ['Kevin Runde'],
+    //     'Web Client' : ['John Rozeske'],
+    //     'Play Testers' : ['John Ottinger', 'Jamie Vrbsky']
+    // }
+
+    handleResult: function(result) {
+        var newResult = {};
+
+        _.each(result, function(foo) {
+            _.each(foo, function(names, header) {
+                newResult[header] = names;
+            });
+        });
+
+        return newResult;
+    },
+
+    onShow: function() {
+        AboutActions.load();
+    },
+
     onLoad: function() {
+        // The credits change very rarely so don't waste RPC's on them.
+        if (this.hasData()) {
+            return;
+        }
+
         server.call({
             module: 'stats',
             method: 'credits',
             params: [],
             addSession: false,
             scope: this,
-            success: this.trigger
+            success: function(result) {
+                this.data = this.handleResult(result);
+                this.trigger(this.data);
+            }
         });
     }
 });
