e2e4015c0f41755d667c34ff5443b4a523502925
1vasari <onevasari@gmail.com>
2015-12-20 15:32:56 +1000

    Did some refactoring of the About window code.

--- app/js/components/window/about.jsx
+++ app/js/components/window/about.jsx
@@ -14,9 +14,11 @@ var CreditsRPCStore = require('js/stores/rpc/stats/credits');
 var Panel = require('js/components/panel');
 
 var About = React.createClass({
+
     mixins: [
         Reflux.connect(ServerRPCStore, 'server')
     ],
+
     render: function() {
         return (
             <div>
@@ -31,69 +33,89 @@ var About = React.createClass({
     }
 });
 
-var NamesList = React.createClass({
+var CreditsSection = React.createClass({
+
     propTypes: {
+        header: React.PropTypes.string.isRequired,
         names: React.PropTypes.arrayOf(React.PropTypes.string)
     },
+
+    getDefaultProps: function() {
+        return {
+            header: '',
+            names: []
+        };
+    },
+
     render: function() {
-        var list = [];
+        return (
+            <li
+                style={{
+                    listStyle: 'decimal outside none'
+                }}
+            >
+                {this.props.header}
 
-        _.each(this.props.names, function(name) {
-            list.push(
-                <li key={name} style={{listStyle: 'disc outside none'}}>
-                    {name}
-                </li>
-            );
-        });
-
-        return <ul style={{paddingLeft: '20px'}}>{list}</ul>;
+                <ul style={{paddingLeft: 20}}>
+                    {
+                        _.map(this.props.names, function(name) {
+                            return (
+                                <li
+                                    key={name}
+                                    style={{
+                                        listStyle: 'disc outside none'
+                                    }}
+                                >
+                                    {name}
+                                </li>
+                            );
+                        })
+                    }
+                </ul>
+            </li>
+        );
     }
 });
 
 var Credits = React.createClass({
+
     mixins: [
         Reflux.connect(CreditsRPCStore, 'credits')
     ],
-    componentDidUpdate: function() {
-        if (this.state.credits.length === 0) {
-            AboutActions.load();
-        }
-    },
+
     render: function() {
-        var credits = [];
-
-        _.each(this.state.credits, function(creditsObject) {
-            _.each(creditsObject, function(names, category) {
-                credits.push(
-                    <li key={category} style={
-                            {listStyle: 'decimal outside none'}}>
-                        {category}
-                        <NamesList names={names} />
-                    </li>
-                );
-            });
-        });
-
-        return <ol style={{paddingLeft: '40px'}}>{credits}</ol>;
+        return (
+            <ol style={{paddingLeft: 40}}>
+                {
+                    _.map(this.state.credits, function(names, header) {
+                        return (
+                            <CreditsSection
+                                key={header}
+                                header={header}
+                                names={names}
+                            />
+                        );
+                    })
+                }
+            </ol>
+        );
     }
 });
 
 var AboutWindow = React.createClass({
+
     mixins: [
         Reflux.connect(AboutWindowStore, 'show')
     ],
+
     render: function() {
         return (
             <Panel title="About" onClose={AboutActions.hide} show={this.state.show}>
-
                 <h2>The Lacuna Expanse</h2>
                 <About />
 
-                <br />
-
                 <h2>Credits</h2>
-                <Credits credits={this.state.credits} />
-
+                <Credits />
             </Panel>
         );
     }
