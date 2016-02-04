fb4f53d2d0262d949742aad5f9a0fcf7915f325b
1vasari <me@1vasari.xyz>
2016-02-04 11:11:05 +1000

    Clean up after logging out. Closes #167 and closes #166

--- app/js/stores/user.js
+++ app/js/stores/user.js
@@ -2,13 +2,15 @@
 
 var Reflux = require('reflux');
 
+var ReactTooltip = require('react-tooltip');
+
 var ChatActions = require('js/actions/menu/chat');
 var MenuActions = require('js/actions/menu');
 var SessionActions = require('js/actions/session');
 var StatusActions = require('js/actions/status');
 var TickerActions = require('js/actions/ticker');
 var UserActions = require('js/actions/user');
-var SessionStore = require('js/stores/session');
+var WindowManagerActions = require('js/actions/menu/windowManager');
 
 var server = require('js/server');
 
@@ -25,7 +27,7 @@ var UserStore = Reflux.createStore({
         server.call({
             module: 'empire',
             method: 'logout',
-            params: [SessionStore.getData()],
+            params: [],
             success: function() {
                 // Here be the traditional code to reset the game...
                 YAHOO.lacuna.Game.Reset();
@@ -38,6 +40,10 @@ var UserStore = Reflux.createStore({
                 MenuActions.hide();
                 TickerActions.stop();
                 ChatActions.hide();
+                WindowManagerActions.hideAllWindows();
+
+                // Hide all our tooltips
+                ReactTooltip.hide();
             }
         });
     }
