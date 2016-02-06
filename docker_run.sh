1bb692f3b7887d6bc60b6b2170f80da8729d9f3c
Iain C Docherty <github@iain-docherty.com>
2016-02-06 16:40:21 +0000

    Revert "Refactoring, largely to rename actions"
    This reverts commit 8c44f16a4c117267f7c8411d606b3701844e8d1e.

--- docker_run.sh
+++
@@ -1,7 +0,0 @@
-docker run -it --rm -p ${TLE_CLIENT_LISTEN:-127.0.0.1}:${TLE_CLIENT_PORT:-8080}:8080  \
-    -v ${PWD}/app:/src/app \
-    -v ${PWD}/index.html:/src/index.html \
-    -v ${PWD}/lacuna/assets:/src/lacuna/assets \
-    --name=tle-client \
-    lacuna/tle-client /bin/bash
-
