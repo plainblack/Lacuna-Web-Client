4b3df11c5303a787f343bcf1a69a6c9c38245f75
1vasari <me@1vasari.xyz>
2016-02-15 22:06:55 +1000

    Premature optimization. Let React handle it.

--- app/js/stores/mixins/stateful.js
+++ app/js/stores/mixins/stateful.js
@@ -26,13 +26,8 @@ var StatefulStore = {
     },
 
     emit : function(newState) {
-        var oldStateStr = JSON.stringify(this.state);
-        var newStateStr = JSON.stringify(newState);
-
-        if (oldStateStr !== newStateStr) {
-            this.setState(newState);
-            this.trigger(this.state);
-        }
+        this.setState(newState);
+        this.trigger(this.state);
     }
 };
 
