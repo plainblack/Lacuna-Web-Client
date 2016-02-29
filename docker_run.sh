20696a1ab7927153e3cec4424292a0e9a2f994ae
Iain C Docherty <iain@iain-docherty.com>
2016-02-29 12:36:41 -0500

    Fixed for docker

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,6 @@
 docker run -it --rm -p ${TLE_CLIENT_LISTEN:-0.0.0.0}:${TLE_CLIENT_PORT:-8080}:8080  \
     -v ${PWD}/app:/src/app \
+    -v ${PWD}/gulp-tasks:/src/gulp-tasks \
     -v ${PWD}/index.html:/src/index.html \
     -v ${PWD}/lacuna/assets:/src/lacuna/assets \
     --name=tle-client \
