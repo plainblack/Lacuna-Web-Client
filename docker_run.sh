02cb4e3c24ee10a6c1d759c6ee2b17009138cd22
Iain C Docherty <iain@iain-docherty.com>
2016-03-05 03:28:33 -0500

    Resolved issue with build not finding index.html

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
-docker run -it --rm -p ${TLE_CLIENT_LISTEN:-0.0.0.0}:${TLE_CLIENT_PORT:-8080}:8080  \
+docker run -it --rm -p ${TLE_CLIENT_LISTEN:-0.0.0.0}:${TLE_CLIENT_PORT:-8080}:80  \
     -v ${PWD}/app:/src/app \
     -v ${PWD}/gulp-tasks:/src/gulp-tasks \
     -v ${PWD}/index.html:/src/index.html \
